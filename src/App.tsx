a34f9166609e67f93dca5b4fb13b6a61860e5f28
Kanghuan Xu <tommyxu@bu.edu>
2025-03-07 16:02:48 -0500

    fix app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./app.css";
+import "./App.css";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
